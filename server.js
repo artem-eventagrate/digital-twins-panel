c333636a319d8b84f50a182ba5958510221365e3
Arraxy <artyom.prilip@gmail.com>
2022-11-24 21:48:43 +0600

    Implemented through node express web-server, added dockerfile

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express'),
+    app = express(),
+    path = require('path');
+
+let serverPort = 8080;
+
+app.use(express.static('public'))
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, 'public', 'index.html')));
+app.listen(serverPort, async () => {
+    console.log("System: Listening on " + serverPort);
+});
+
+process.on('uncaughtException', err => {
+    console.error(err && err.stack);
+});
